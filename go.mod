7984ca40dbaa99585dad0f3bd377c497f8d1310d
Victor Parmar <vic@smalldata.tech>
2019-03-15 16:20:49 +0100

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/smalldatatech/hello-world-go
+
+go 1.12
+
+require github.com/smalldatatech/hello-world-mod v1.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	helloworld "github.com/smalldatatech/hello-world-mod"
+)
+
+func main() {
+	fmt.Println(helloworld.Hello("Snoop"))
+}
